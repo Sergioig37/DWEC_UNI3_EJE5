61c51067de04f35ef8e71c6ca59c74ef76496502
Sergioig37 <sergioig37@educastur.es>
2023-11-06 13:20:31 +0100

    empezao, a ver como lo acabo

---
+++ js/propuesto5.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", cargarPagina());
+
+
+function pasarAMayus(){
+
+}
+
+function salCampoTexto(texto){
+    var mayus = texto.select();
+    mayus.toUpperCase();
+    texto.select() = mayus;
+}
+
+
+function cargarPagina(){
+
+}
