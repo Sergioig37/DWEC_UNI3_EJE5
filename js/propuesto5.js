b4e43bf5d1b873c0ecebc53e2d64b5e7da818337
Sergioig37 <sergioig37@educastur.es>
2023-11-06 14:19:35 +0100

    mas o menos acabado

--- js/propuesto5.js
+++ js/propuesto5.js
@@ -1,17 +1,49 @@
-window.addEventListener("load", cargarPagina());
-
+window.addEventListener("load", cargarPagina);
+const formulario = document.getElementById("formulario").elements;
+const passwrd = document.getElementById("password").value;
 
 function pasarAMayus(){
 
 }
 
-function salCampoTexto(texto){
-    var mayus = texto.select();
-    mayus.toUpperCase();
-    texto.select() = mayus;
+function salCampoTexto(evento){
+    var elem = evento.target;
+    elem.value = elem.value.toUpperCase();
+    
+    
 }
 
+function salPasswrd2(evento){
+    var elem = evento.target;
+    var valido = false;
+    if(passwrd==elem.value&&elem.maxLength<=8){
+       valido = true;
+}
+return valido;
+}
+
+function validarFormulario() {
+    console.log('Contraseñas coinciden y tienen al menos 8 caracteres.');
+    console.log('Datos del formulario:');
+    console.log('Nombre: ' + document.getElementById("nombre").value);
+    console.log('Apellido: ' + document.getElementById("apellido").value);
+    console.log('Correo Electrónico: ' + correoInput.value);
+    console.log('Contraseña: ' + contrasena1);
+}
 
 function cargarPagina(){
+    
+    Array.from(formulario).forEach(element => {
+        if(element.type=="text"){
+            element.addEventListener("blur", salCampoTexto, false);
+        }
+        if(element.getElementById=="password2"){
+            element.addEventListener("blur", salPasswrd2, false);
+        }
+
+        element.getElementById("enviar").addEventListener("click", validarFormulario,false);
+    });
+
+
 
 }
